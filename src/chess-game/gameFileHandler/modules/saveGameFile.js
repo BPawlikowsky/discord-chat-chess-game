ffdc4148c7335e95dbbab0318c79948e5fd3636c
B. Pawlikowsky <bartospawlos@gmail.com>
2022-01-26 16:36:42 +0100

    eslint fix

--- src/chess-game/gameFileHandler/modules/saveGameFile.js
+++ src/chess-game/gameFileHandler/modules/saveGameFile.js
@@ -1,4 +1,4 @@
-const { createWriteFile } = require("./createWriteFile");
+const { createWriteFile } = require('./createWriteFile');
 
 exports.saveGameFile = async (gameObj) => {
 	const data = JSON.stringify(gameObj);
